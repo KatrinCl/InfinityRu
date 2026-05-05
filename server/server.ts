630990705eb4bf6a649f2ec3017b409635f74d89
KatrinCl <rybina.ekaterina.93@mail.ru>
2026-05-05 11:50:32 +0300

    server

--- server/server.ts
+++ server/server.ts
@@ -7,30 +7,28 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import adminRouter from "./routes/adminRoute.js";
-// import { toNodeHandler } from "better-auth/node";
-// import { auth } from "./lib/auth.js";
-// import projectRouter from "./routes/projectRoutes.js";
-// import { stripeWebhook } from "./controllers/stripeWebhook.js";
 
 const app = express();
 
 const port = Number(process.env.PORT) || 4000;
 
 const corsOptions = {
-    origin: process.env.TRUSTED_ORIGINS?.split(',') || ["http://localhost:5173"],
+    origin: process.env.TRUSTED_ORIGINS?.split(',') || ["http://localhost:5173", "https://infinitytsru.vercel.app"],
     credentials: true
 }
 
 app.use(cors(corsOptions))
 app.use(cookieParser())
-// app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 
-// app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.post('/api/order/webhook/stripe', express.raw({type: 'application/json'}), (req: any, res: any) => {
+    const { stripeWebhook } = require('./controllers/orderController.js');
+    stripeWebhook(req, res);
+});
 
 app.use(express.json({limit: '50mb'}));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+    res.send('Server is running at port ' + port);
 });
 
 app.use('/api/user', userRouter)
@@ -39,8 +37,7 @@ app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
 app.use('/api/admin', adminRouter)
 app.use('/uploads', express.static('uploads'))
-// app.use('/api/project', projectRouter)
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`✅ Server is running at http://localhost:${port}`);
 });
